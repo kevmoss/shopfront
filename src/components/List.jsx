902c00afe575b97182fb94a77c2d1b6e7356c5d9
Kevin Moss <kev_moss@hotmail.com>
2016-08-03 11:54:02 +0100

    list items added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,50 @@
 const React = require('react');
 
+var items = [
+  {
+    name: "Glass",
+    price: "£2",
+    qty: 2
+  },
+  {
+    name: "Coasters",
+    price: "£2",
+    qty: 2
+  },
+  {
+    name: "Straws",
+    price: "£1",
+    qty: 5
+  },
+  {
+    name: "Cups",
+    price: "£2.50",
+    qty: 4
+  },
+  {
+    name: "Spoons",
+    price: "£1.50",
+    qty: 8
+  }
+];
+
 var List = React.createClass({
+
   render: function () {
+    var listnodes = items.map(function (item, index){
+      return (
+        <div key={index}>
+        <h1>{item.name}</h1>
+        <p>{item.price}</p>
+        <p>{item.qty}</p>
+      </div>
+      )
+    });
+
     return (
-      <h2>Hi I'm a list</h2>
+      <div>
+    {listnodes}
+      </div>
     );
   }
 });
